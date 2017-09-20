7950b60ea5d42c8bbf3393acb8432020b5007343
Kalle <k.oberkoxholt@osram.com>
2017-09-20 16:22:01 +0200

    structure

---
+++ src/controller/planets.js
@@ -0,0 +1,38 @@
+/**
+ * The controller script contains everything needed for an endpoint.
+ * This means route settings, validation schemas and the handler functions are placed here.
+ * 
+ * If you need much logic to serve the endpoint it is a good idea to write it in another script in an extra subfolder of ../src. 
+ * If only need some small functions, which are also only used for this endpoint, you can also integrate them in this file.
+ */
+
+'use strict';
+
+const Joi = require('joi');
+
+
+/**
+ * Joi Validation Schemas 
+ */
+
+
+
+/**
+ * Functions
+ */
+
+
+
+/**
+ * Handler functions
+ */
+
+const getPlanetByName = (request, reply) => { //I name the handlerfunctions by <method><Endpoint>[By<path>]
+
+}
+
+/**
+ * route configuration
+ */
+
+
